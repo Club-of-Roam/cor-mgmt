69caf04ab25afe6ca25a6f7f051266bcdfa832d9
michele <m.boschert@arcor.de>
2025-07-29 19:55:19 +0200

    fixed h3-mgmt-isotope.js

--- js/h3-mgmt-isotope.js
+++ js/h3-mgmt-isotope.js
@@ -1,70 +1,65 @@
-/* global IsotopeParams */
-
-jQuery('#stages-filters a').on('click', () => {
+(function ($) { // closure
     'use strict';
-    const selector = jQuery(this).attr('data-filter');
-    jQuery('#stages-container').isotope({ filter: selector });
-    return false;
-});
+    $(() => {
+        $('#stages-filters a').on('click', () => {
+            const selector = $(this).attr('data-filter');
+            $('#stages-container').isotope({ filter: selector });
+            return false;
+        });
 
-jQuery('#country-filters a').on('click', () => {
-    'use strict';
-    const selector = jQuery(this).attr('data-filter');
-    jQuery('#stages-container').isotope({ filter: selector });
-    return false;
-});
+        $('#country-filters a').on('click', () => {
+            const selector = $(this).attr('data-filter');
+            $('#stages-container').isotope({ filter: selector });
+            return false;
+        });
 
-jQuery('#stages-container').isotope({
-    getSortData: {
-        route: '.route',
-        stage: '.stage parseInt',
-        country: '.country',
-    },
-});
+        $('#stages-container').isotope({
+            getSortData: {
+                route: '.route',
+                stage: '.stage parseInt',
+                country: '.country',
+            },
+        });
 
-jQuery('#sort-by a').on('click', () => {
-    'use strict';
-    const sortName = jQuery(this).attr('href').slice(1);
-    jQuery('#stages-container').isotope({ sortBy: sortName });
-    return false;
-});
+        $('#sort-by a').on('click', () => {
+            const sortName = $(this).attr('href').slice(1);
+            $('#stages-container').isotope({ sortBy: sortName });
+            return false;
+        });
 
-jQuery('#teams-route-filters a').on('click', () => {
-    'use strict';
-    const selector = jQuery(this).attr('data-filter');
-    jQuery('#teams-container').isotope({ filter: selector });
-    return false;
-});
+        $('#teams-route-filters a').on('click', () => {
+            const selector = $(this).attr('data-filter');
+            $('#teams-container').isotope({ filter: selector });
+            return false;
+        });
 
-jQuery('#teams-other-filters a').on('click', () => {
-    'use strict';
-    const selector = jQuery(this).attr('data-filter');
-    jQuery('#teams-container').isotope({ filter: selector });
-    return false;
-});
+        $('#teams-other-filters a').on('click', () => {
+            const selector = $(this).attr('data-filter');
+            $('#teams-container').isotope({ filter: selector });
+            return false;
+        });
 
-jQuery('#teams-container').isotope({
-    getSortData: {
-        route: '.team-overview-route',
-        teamname: '.team-name',
-        sponsorcount: '.sponsor-count parseInt',
-    },
-});
+        $('#teams-container').isotope({
+            getSortData: {
+                route: '.team-overview-route',
+                teamname: '.team-name',
+                sponsorcount: '.sponsor-count parseInt',
+            },
+        });
 
-jQuery('#teams-sort-by a').on('click', () => {
-    'use strict';
-    const sortName = jQuery(this).attr('href').slice(1);
-    jQuery('#teams-container').isotope({ sortBy: sortName });
-    return false;
-});
+        $('#teams-sort-by a').on('click', () => {
+            const sortName = $(this).attr('href').slice(1);
+            $('#teams-container').isotope({ sortBy: sortName });
+            return false;
+        });
 
-jQuery(window).on('load', () => {
-    'use strict';
-    jQuery('#teams-container').isotope();
-});
+        $('.isotope-wrap .jumper').on('click', () => {
+            const teamID = $('.isotope-wrap .team-selector').val();
+            window.location.search = `id=${teamID}`;
+        });
 
-jQuery('.isotope-wrap .jumper').on('click', () => {
-    'use strict';
-    const teamID = jQuery('.isotope-wrap .team-selector').val();
-    window.location = `${IsotopeParams.redirect}?id=${teamID}`;
-});
+        $(window).on('load', () => {
+            $('#teams-container').isotope();
+        });
+    });
+})(jQuery); // closure
