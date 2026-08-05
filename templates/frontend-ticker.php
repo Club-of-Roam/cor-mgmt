acd1e474ed55fa7b60c6eb7e6bccda79c60c75ba
michele <m.boschert@arcor.de>
2026-08-05 19:20:50 +0200

    removed unused function autoRotateImage which produced errors because of redeclaration

--- templates/frontend-ticker.php
+++ templates/frontend-ticker.php
@@ -178,26 +178,3 @@ if ( isset ( $messages ) && ! empty( $messages ) ) {
 
 	} // foreach message
 } // if ! empty
-
-function autoRotateImage($image) { 
-    $orientation = $image->getImageOrientation(); 
-
-    switch($orientation) { 
-        case imagick::ORIENTATION_BOTTOMRIGHT: 
-            $image->rotateimage("#000", 180); // rotate 180 degrees 
-        break; 
-
-        case imagick::ORIENTATION_RIGHTTOP: 
-            $image->rotateimage("#000", 90); // rotate 90 degrees CW 
-        break; 
-
-        case imagick::ORIENTATION_LEFTBOTTOM: 
-            $image->rotateimage("#000", -90); // rotate 90 degrees CCW 
-        break; 
-    } 
-
-    // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image! 
-    $image->setImageOrientation(imagick::ORIENTATION_TOPLEFT); 
-}
-
-?>
